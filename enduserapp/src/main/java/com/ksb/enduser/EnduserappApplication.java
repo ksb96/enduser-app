c1e61763498ff4f7ad21cab01063ea1f26f718b9
ksb96 <52975077+ksb96@users.noreply.github.com>
2023-11-18 14:03:32 +0530

    final consumer code push

---
+++ enduserapp/src/main/java/com/ksb/enduser/EnduserappApplication.java
@@ -0,0 +1,13 @@
+package com.ksb.enduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnduserappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnduserappApplication.class, args);
+	}
+
+}
---
+++ enduserapp/src/test/java/com/ksb/enduser/EnduserappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksb.enduser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnduserappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
